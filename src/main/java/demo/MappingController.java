1ea59b904437d7a06006627b2ec486c21b0f2ba4
Marvin Ebert <mar.ebert.22@lehre.mosbach.dhbw.de>
2023-10-18 23:25:46 +0200

    Added all API-Design funktionen ohne Impl

--- src/main/java/demo/MappingController.java
+++ src/main/java/demo/MappingController.java
@@ -1,6 +1,7 @@
 package demo;
 
 import demo.data.api.*;
+import demo.data.impl.*;
 
 import demo.model.SendBackToken;
 import org.springframework.http.HttpStatus;
@@ -16,12 +17,13 @@ import java.util.logging.Logger;
 @RestController
 @RequestMapping("/api/v1.0")
 public class MappingController {
-    //Kommentar entfernen für Datenspeicherung in DB
-    //UserManager userManager = PostgresDBUserManagerImpl.getPostgresDBUserManagerImpl();
+
     //sehr wichtig, nach API First Prinzip hier vorzugehen
-    //ToDo: API_Design Tabelle aus GoogleDrive Ordner anschauen und demnach vorgehen
-    // bzw. Methoden erstellen, bei Hartwig nachschauen wegen genauen Bezeichnungen
     //Testen der HTTP:Resquest mit Prestige.dev und den passenden URL und HTTP-Aufrufe
+    //folgender UserManager legt die User in der DB an und bearbeitet ihre Daten
+    UserManager userManager = PostgresDBUserManagerImpl.getPostgresDBUserManagerImpl();
+    //evtl. noch ein shoppingListManager der sich um alles mit der ShoppingList kümmert
+    //ähnlich dem Prinzip von Hartwig mit TaskManager
 
     /*
      GET-Methode
@@ -30,40 +32,124 @@ public class MappingController {
     @GetMapping("/auth")
 
     public String getInfo(@RequestParam(value = "name", defaultValue = "Student") String name) {
-        Logger.getLogger("MappingController").log(Level.INFO,"MappingController auth " + name);
+        Logger.getLogger("MappingController").log(Level.INFO, "MappingController auth " + name);
         return "im alive guys";
     }
+
     /*
      Post-Methode
       für den Login eines Users in das System
      */
-@PostMapping(
-        path=("/auth/login"),
-        consumes ={MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
-)
-@ResponseStatus(HttpStatus.OK)
-public SendBackToken userLogin(@RequestBody User user){
-    //To-do: Check Verification of the token
-    return new SendBackToken("jhnaosvgioa gvi",67978);
-}
-
-@DeleteMapping (
-        path=("/auth/logoff"),
-        consumes ={MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
+    @PostMapping(
+            path = ("/auth/login"),
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
     )
-    public String userLogoff(){
+    @ResponseStatus(HttpStatus.OK)
+    public SendBackToken userLogin(@RequestBody User user) {
+        //To-do: Check Verification of the token
+        return new SendBackToken("jhnaosvgioa gvi", 67978);
+    }
+
+    @DeleteMapping(
+            path = ("/auth/logoff"),
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
+    )
+    public String userLogoff() {
 
         return "Log off was succesfull";
     }
 
+    /*
+     POST-Methode, die es ermöglicht das der User sich
+     einmalig im System registrieren kann
+     */
+    @PostMapping(
+            path = ("/user"),
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
+    )
+    @ResponseStatus(HttpStatus.OK)
+    public SendBackToken registerUser() {
 
+        return new SendBackToken("uavoiggpvagiv", 360);
+    }
+
+    /*
+     GET-Methode, die es ermöglicht das der User
+     seine Daten zurückbekommt
+     */
+    @GetMapping(
+            path = ("/user"),
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
+    )
+    public UserImpl getUserData() {
+
+        return new UserImpl("tester", "");
+    }
+
+    /*
+ DELETE-Methode, die es ermöglicht das
+ der Nutzer seinen Account löschen kann
+ */
+    @DeleteMapping(
+            path = ("/user"),
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
+    )
+    public String deleteUser() {
+
+        return "User got deleted with the follwing name: Test";
+    }
+
+    /*
+ GET-Methode, die es ermöglicht das der Admin
+ alle User mit ihren Daten angezeigt bekommt
+ */
     @GetMapping(
             path = ("/user/all"),
             consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
     )
-    public String getAllUsers(){
+    public String getAllUsers() {
 
-    return "Hallo ich funktioniere";
+        return "Hallo, Test";
     }
+    /*
+    GET-Methode, die dem Nutzer ermöglicht seine aktuelle
+    Einkaufsliste angezeigt zu bekommen
+     */
+
+    @GetMapping(
+            path = ("/shoppinglist"),
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_JSON_VALUE}
+    )
+    public String getShoppinglist() {
+
+        return "hier die Shoppinglist";
+    }
+
+    /*
+    Put-Methode, die dem Nutzer ermöglicht seine
+    Einkaufsliste mit Zutaten zu füllen
+     */
+    @PutMapping(
+            path = ("/shoppinglist"),
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_JSON_VALUE}
+    )
+    public String addIngredient() {
+
+        return "Ingredient got added to the shopping List";
+    }
+
+    /*
+    Delete-Methode, die dem Nutzer ermöglicht seine
+    Einkaufsliste zu löschen
+     */
+    @DeleteMapping(
+            path = ("/shoppinglist"),
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_JSON_VALUE}
+    )
+    public String deleteShoppinglist() {
+
+        return "shoppingList got deleted";
+    }
+
 
 }
