88024698119411075a70904e58c04798d5f21b56
Marvin Ebert <mar.ebert.22@lehre.mosbach.dhbw.de>
2023-10-22 10:29:42 +0200

    changed readUsers 1 to i in while

--- src/main/java/demo/data/impl/PropertyFileUserManagerImpl.java
+++ src/main/java/demo/data/impl/PropertyFileUserManagerImpl.java
@@ -63,7 +63,7 @@ public class PropertyFileUserManagerImpl implements UserManager {
         int i = 1;
         try {
             properties.load(new FileInputStream(userPropertyFile));
-            while (properties.containsKey("user." + 1 + ".firstName")) {
+            while (properties.containsKey("user." + i + ".firstName")) {
                 userData.add(
                         new UserImpl(
                                 properties.getProperty("user." + i + ".firstName"),
