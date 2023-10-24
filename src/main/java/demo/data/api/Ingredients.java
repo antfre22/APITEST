39bbfe69e2956db16bf9580c1bcc48996e3a6817
sen.hasic.22 <sen.hasic.22@lehre.mosbach.dhbw.de>
2023-10-24 16:09:36 +0200

    getName und setName in Override in der IngredientsImpl

--- src/main/java/demo/data/api/Ingredients.java
+++ src/main/java/demo/data/api/Ingredients.java
@@ -3,7 +3,10 @@ package demo.data.api;
 public interface Ingredients {
     String getName();
 
+    void setName(String name);
+
     float getQuantity();
 
 
+    void setQuantity(float quantity);
 }
