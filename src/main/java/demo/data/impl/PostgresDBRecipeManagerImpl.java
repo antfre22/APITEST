d6b51d3565eeeac33522577780aee86e40ba9151
Marvin Ebert <mar.ebert.22@lehre.mosbach.dhbw.de>
2023-11-04 10:52:38 +0100

    changed last attribut at insert into recipes

--- src/main/java/demo/data/impl/PostgresDBRecipeManagerImpl.java
+++ src/main/java/demo/data/impl/PostgresDBRecipeManagerImpl.java
@@ -58,7 +58,7 @@ public class PostgresDBRecipeManagerImpl implements RecipeManager{
             String udapteSQL = "INSERT into recipes (recipeName, datum, TokenOfUser) VALUES (" +
                     "'" + recipeName+ "', "
                     + "'" + datum + "')"
-                    + "notoken";
+                    + "'notoken'";
 
             stmt.executeUpdate(udapteSQL);
 
